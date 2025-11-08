34a32b92c8e5dcaba5ceeb54aa8b8da8764f8688
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-11-08 11:58:08 +0100

    Line Coverage, Mutation Coverage,Test Strength a 100%

--- autoAssignStudents/src/test/java/delft/AutoAssignerTest.java
+++ autoAssignStudents/src/test/java/delft/AutoAssignerTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class AutoAssignerTest {
 
@@ -46,6 +44,25 @@ class AutoAssignerTest {
     public void goodCreationStudentTest(){
         students.add(Louis);
         Student louisTemp = students.get(0);
+        Student Louis2 =  new Student(1, "Louis", "Louis@gmail.com");
+        Student Louis3 =  new Student(2, "Louis", "Louis@gmail.com");
+        Student Louis4 =  new Student(1, "Loui_s", "Louis@gmail.com");
+        Student Louis5 =  new Student(1, "Louis", "Louis_B@gmail.com");
+
+        Student Houria = new Student(5, "Houria", "Houria@gmail.com");
+        assertTrue(Houria.equals(Houria));
+        assertFalse(Houria.equals(null));
+        Student Julien = null;
+        assertNull(Julien);
+
+        assertTrue(Louis.equals(louisTemp));
+        assertFalse(Louis.equals("Not the same type"));
+
+        assertEquals(Louis2, Louis);
+        assertNotEquals(Louis3, Louis);
+        assertNotEquals(Louis4, Louis);
+        assertNotEquals(Louis5, Louis);
+
         assertEquals(louisTemp.getName(), "Louis");
         assertEquals(louisTemp.getId(), 1);
         assertEquals(louisTemp.getEmail(), "Louis@gmail.com");
@@ -64,12 +81,87 @@ class AutoAssignerTest {
 
     @Test
     public void goodCreationWorkshopTest(){
+
         workshops.add(workshop1);
-        Workshop workshopTemp = new Workshop(4, "workshop1", spotsPerDate1);
+        Workshop workshopTemp = new Workshop(1, "workshop1", spotsPerDate1);
         workshops.add(workshopTemp);
+
+        assertNotNull(workshopTemp.getSpotsPerDate());
+
+        assertFalse(workshopTemp.getSpotsPerDate().isEmpty());
+
+        assertEquals("workshop1", workshopTemp.getName());
+        assertEquals(1, workshopTemp.getId());
+        assertEquals(1, workshop1.getId());
+
         assertEquals(workshopTemp.getSpotsPerDate(), workshop1.getSpotsPerDate());
     }
 
+    @Test
+    public void goodCreationWorkshopAndEqualsTest(){
+        Workshop workshopTemp = new Workshop(3, "workshop3", spotsPerDate3);
+
+        Workshop workshopTest1 = new Workshop(4, "workshop3", spotsPerDate3);
+        Workshop workshopTest2 = new Workshop(3, "workshop4", spotsPerDate3);
+        Workshop workshopTest3 = new Workshop(3, "workshop3", spotsPerDate2);
+
+        assertTrue(workshopTemp.equals(workshopTemp));
+
+        assertEquals(workshopTemp.hashCode(),  workshopTemp.hashCode());
+        assertNotEquals(0, workshopTemp.hashCode());
+
+        assertFalse(workshopTemp.equals("not the same type"));
+
+        assertNotEquals(workshopTemp, workshopTest1);
+        assertNotEquals(workshopTemp, workshopTest2);
+        assertNotEquals(workshopTemp, workshopTest3);
+    }
+
+    @Test
+    public void goodCreationWorkshopOnFilterTest(){
+        ZonedDateTime dateTemp = date(2027, 10, 06, 12, 32);
+        Map<ZonedDateTime, Integer> spotsPerDate = Map.of(dateTemp, 2);
+
+        Workshop workshopTemp = new Workshop(1, "workshop1", spotsPerDate);
+        assertEquals(dateTemp, workshopTemp.getNextAvailableDate());
+    }
+
+    @Test
+    public void goodCreationWorkshopDateWithNoPlaceTest(){
+        ZonedDateTime dateTemp = date(2027, 10, 06, 12, 32);
+        Map<ZonedDateTime, Integer> spotsPerDate = Map.of(dateTemp, 0);
+
+        Workshop workshopTemp = new Workshop(1, "workshop1", spotsPerDate);
+        assertThrows(NoSuchElementException.class, workshopTemp::getNextAvailableDate); //Aider par chatgpt pour trouver comment jeter des exceptions
+    }
+
+    @Test
+    public void goodCreationWorkshopAndTakeTheGoodOneTest(){
+        ZonedDateTime dateTemp = date(2027, 10, 06, 12, 32);
+        ZonedDateTime dateTest1 = date(2027, 10, 03, 12, 32);
+        ZonedDateTime dateTest2= date(2027, 10, 02, 12, 32);
+        Map<ZonedDateTime, Integer> spotsPerDate = new HashMap<>();
+
+        spotsPerDate.put(dateTemp, 0);
+        spotsPerDate.put(dateTest1, 2);
+        spotsPerDate.put(dateTest2, -1);
+
+        Workshop workshopTemp = new Workshop(1, "workshop1", spotsPerDate);
+        assertEquals(dateTest1, workshopTemp.getNextAvailableDate());
+    }
+
+    @Test
+    public void WorkshopAndCaseWhereThereIsNullTest(){
+        ZonedDateTime dateTemp = date(2027, 10, 06, 12, 32);
+        Map<ZonedDateTime, Integer> spotsPerDate = new HashMap<>();
+        spotsPerDate.put(dateTemp, 0);
+        Workshop workshopTemp = new Workshop(1, "workshop1", spotsPerDate);
+
+        assertFalse(workshopTemp.equals(null));
+        Workshop workshopNull = null;
+        assertNull(workshopNull);
+
+    }
     // ———————————————— Test on map —————————————————
 
     @Test
@@ -140,6 +232,19 @@ class AutoAssignerTest {
             assertEquals(listeNameOfStudent.get(i), student.getName());
             i++;
         }
+    }
+
+    @Test
+    public void testOnFormatOfDateWithStudentInWorkshop() {
+        workshops.add(workshop1);
+        students.add(Louis);
+        AutoAssigner autoAssigner = new AutoAssigner();
+        AssignmentsLogger assignmentsLogger = autoAssigner.assign(students, workshops);
+        List<String> assignments= assignmentsLogger.getAssignments();
+        assertEquals(assignments.size(), 1);
+        System.out.println(assignments.get(0));
+        assertEquals(assignments.get(0), "workshop1,Louis,04/11/2025 10:30");
+
     }
 
 }
